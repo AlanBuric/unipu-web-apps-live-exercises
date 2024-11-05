681f8455b7a6288ec77ac0e5918822e4e044ff73
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-11-05 18:59:00 +0100

    refactor(git): replace V2 with V3, rename to Pizzeria

--- V2/src/server.ts
+++
@@ -1,7 +0,0 @@
-import application from "./application.js";
-
-const port = 3000;
-
-application.listen(port, () =>
-  console.log(`Express server is up and running on http://localhost:${port}`)
-);
--- V2/src/service/order.ts
+++
@@ -1,9 +0,0 @@
-import { PizzaOrder } from "../types/types.js";
-
-const orders: PizzaOrder[] = [];
-
-export default class OrderService {
-  static addOrder(order: PizzaOrder) {
-    orders.push(order);
-  }
-}
