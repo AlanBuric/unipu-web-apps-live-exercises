c8edc9b84c729d6f98be96b71426d25718ca554a
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-11-07 00:46:51 +0100

    fix: router bugs discovered during tests
    Also convert apostrophes to double quotation marks

--- real-estate-app/src/routes/offer/router.ts
+++ real-estate-app/src/routes/offer/router.ts
@@ -1,5 +1,5 @@
 import {Router, Response, Request} from "express";
-import {body, matchedData, param} from 'express-validator';
+import {body, matchedData, param} from "express-validator";
 import handleValidationResult from "../../util/validation.js";
 import OfferService from "./service.js";
 import {Offer} from "../../types/types.js";
@@ -8,35 +8,36 @@ import PropertyService from "../property/service.js";
 
 function createOfferValidationChain() {
     return [
-        body('propertyId')
-            .isUUID().withMessage('ID nekretnine mora biti valjani UUID')
-            .custom(({value}) => PropertyService.getPropertyById(value))
-            .withMessage(({value}) => `Nekretnina s ID-jem ${value} ne postoji`),
-        body('customerFirstName')
-            .isString().withMessage('Ime kupca mora biti tekstualni string')
-            .isLength({min: 1}).withMessage('Ime kupca je obavezno polje'),
-        body('customerLastName')
-            .isString().withMessage('Prezime kupca mora biti tekstualni string')
-            .isLength({min: 1}).withMessage('Prezime kupca je obavezno polje'),
-        body('offerPrice')
-            .isFloat({min: 0}).withMessage('Ponuđena cijena mora biti pozitivan broj'),
-        body('customerPhone')
-            .isString().withMessage('Broj telefona kupca mora biti tekstualni string')
-            .matches(/^\+?[0-9]{7,15}$/).withMessage('Broj telefona mora biti valjan')
+        body("propertyId")
+            .isUUID().withMessage("ID nekretnine mora biti valjani UUID")
+            .custom((input) => PropertyService.getPropertyById(input))
+            .withMessage((value) => `Nekretnina s ID-em ${value} ne postoji`),
+        body("customerFirstName")
+            .isString().withMessage("Ime kupca mora biti tekstualni string")
+            .isLength({min: 1}).withMessage("Ime kupca je obavezno polje"),
+        body("customerLastName")
+            .isString().withMessage("Prezime kupca mora biti tekstualni string")
+            .isLength({min: 1}).withMessage("Prezime kupca je obavezno polje"),
+        body("offerPrice")
+            .isFloat({min: 0}).withMessage("Ponuđena cijena mora biti pozitivan broj"),
+        body("customerPhone")
+            .isString().withMessage("Broj telefona kupca mora biti tekstualni string")
+            .isMobilePhone("any").withMessage("Broj telefona mora biti valjan")
     ]
 }
 
 const mandatoryFieldValidation = createOfferValidationChain();
 const optionalFieldValidation = createOfferValidationChain().map(chain => chain.optional());
 
-const OfferRouter = Router().post("",
-    mandatoryFieldValidation,
-    handleValidationResult,
-    (request: Request<{}, {}, Offer>, response: Response) => {
-        const id = OfferService.addOffer(matchedData(request));
+const OfferRouter = Router()
+    .post("",
+        mandatoryFieldValidation,
+        handleValidationResult,
+        (request: Request<{}, {}, Offer>, response: Response) => {
+            const id = OfferService.addOffer(matchedData(request));
 
-        response.sendStatus(StatusCodes.CREATED).send({id});
-    })
+            response.status(StatusCodes.CREATED).send({id});
+        })
     .get("", (request: Request, response: Response): any => response.send(OfferService.getAllOffers()))
     .use("/:id",
         param("id").isUUID().withMessage("Nepravilan UUID ponude"),
@@ -50,6 +51,7 @@ const OfferRouter = Router().post("",
                 })
             .put("",
                 mandatoryFieldValidation,
+                handleValidationResult,
                 (request: Request, response: Response) => {
                     const {id, ...data} = matchedData(request);
                     const updatedOffer = OfferService.updateOfferById(id, data);
@@ -58,6 +60,7 @@ const OfferRouter = Router().post("",
                 })
             .patch("",
                 optionalFieldValidation,
+                handleValidationResult,
                 (request: Request, response: Response) => {
                     const {id, ...data} = matchedData(request);
                     const updatedOffer = OfferService.updateOfferById(id, data);
--- real-estate-app/src/routes/property/router.ts
+++ real-estate-app/src/routes/property/router.ts
@@ -1,54 +1,58 @@
-import {Router, Request, Response} from 'express';
-import {body, matchedData, param} from 'express-validator';
-import handleValidationResult from '../../util/validation.js';
-import PropertyService from './service.js';
-import {StatusCodes} from 'http-status-codes';
+import {Router, Request, Response} from "express";
+import {body, matchedData, param} from "express-validator";
+import handleValidationResult from "../../util/validation.js";
+import PropertyService from "./service.js";
+import {StatusCodes} from "http-status-codes";
 import {Property} from "../../types/types.js";
 
 const createPropertyValidationChain = () => [
-    body('name')
-        .isString().withMessage('Naziv mora biti tekstualni string')
-        .isLength({min: 1}).withMessage('Naziv je obavezno polje'),
-    body('description')
-        .isString().withMessage('Opis mora biti tekstualni string')
-        .isLength({min: 1}).withMessage('Opis je obavezno polje'),
-    body('price')
-        .isFloat({min: 0}).withMessage('Cijena mora biti pozitivan broj'),
-    body('location')
-        .isString().withMessage('Lokacija mora biti tekstualni string')
-        .isLength({min: 1}).withMessage('Lokacija je obavezno polje'),
-    body('numberOfRooms')
-        .isInt({min: 0}).withMessage('Broj soba mora biti cijeli broj veći ili jednak nuli'),
-    body('area')
-        .isFloat({min: 0}).withMessage('Površina mora biti pozitivan broj')
+    body("name")
+        .isString().withMessage("Naziv mora biti tekstualni string")
+        .isLength({min: 1}).withMessage("Naziv je obavezno polje"),
+    body("description")
+        .isString().withMessage("Opis mora biti tekstualni string")
+        .isLength({min: 1}).withMessage("Opis je obavezno polje"),
+    body("price")
+        .isFloat({min: 0}).withMessage("Cijena mora biti pozitivan broj"),
+    body("location")
+        .isString().withMessage("Lokacija mora biti tekstualni string")
+        .isLength({min: 1}).withMessage("Lokacija je obavezno polje"),
+    body("numberOfRooms")
+        .isInt({min: 0}).withMessage("Broj soba mora biti cijeli broj veći ili jednak nuli"),
+    body("area")
+        .isFloat({min: 0}).withMessage("Površina mora biti pozitivan broj")
 ];
 
-const PropertyRouter = Router().post('',
-    createPropertyValidationChain(),
-    handleValidationResult,
-    (request: Request<{}, {}, Property>, response: Response) => {
-        const id = PropertyService.addProperty(matchedData(request));
+const mandatoryFieldValidation = createPropertyValidationChain();
+const optionalFieldValidation = createPropertyValidationChain().map(chain => chain.optional());
 
-        response.status(StatusCodes.CREATED).send({id});
-    })
-    .get('', (request: Request, response: Response) => {
+const PropertyRouter = Router()
+    .post("",
+        mandatoryFieldValidation,
+        handleValidationResult,
+        (request: Request<{}, {}, Property>, response: Response) => {
+            const id = PropertyService.addProperty(matchedData(request));
+
+            response.status(StatusCodes.CREATED).send({id});
+        })
+    .get("", (request: Request, response: Response) => {
         const properties = PropertyService.getAllProperties();
 
         response.send(properties);
     })
-    .use('/:id',
-        param('id').isUUID().withMessage('Nepravilan UUID nekretnine'),
+    .use("/:id",
+        param("id").isUUID().withMessage("Nepravilan UUID nekretnine"),
         handleValidationResult,
         Router({mergeParams: true})
-            .get('',
+            .get("",
                 (request: Request, response: Response) => {
                     const {id} = matchedData(request);
                     const property = PropertyService.getPropertyById(id);
 
                     response.send(property);
                 })
-            .put('',
-                createPropertyValidationChain(),
+            .put("",
+                mandatoryFieldValidation,
                 handleValidationResult,
                 (request: Request, response: Response) => {
                     const {id, ...data} = matchedData(request);
@@ -56,8 +60,8 @@ const PropertyRouter = Router().post('',
 
                     response.send(updatedProperty);
                 })
-            .patch('',
-                createPropertyValidationChain().map(chain => chain.optional()),
+            .patch("",
+                optionalFieldValidation,
                 handleValidationResult,
                 (request: Request, response: Response) => {
                     const {id, ...data} = matchedData(request);
@@ -65,7 +69,7 @@ const PropertyRouter = Router().post('',
 
                     response.send(updatedProperty);
                 })
-            .delete('',
+            .delete("",
                 (request: Request, response: Response) => {
                     const {id} = matchedData(request);
 
