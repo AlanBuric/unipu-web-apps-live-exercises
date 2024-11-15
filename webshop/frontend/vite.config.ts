f6c4982e412ba7016a1f3efc36ead11496fd6de1
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2024-11-15 22:36:47 +0100

    feat: try to connect backend TS types to frontend

--- webshop/frontend/vite.config.ts
+++ webshop/frontend/vite.config.ts
@@ -1,18 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@shared-types": path.resolve(__dirname, "../backend/src/types/*")
+    }
+  }
+});
